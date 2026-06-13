36ae00506ed82ff1f44974c5280eb99d5835ce36
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-13 17:32:28 +0900

    Enable Korean Japanese English language switcher

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import InstagramQuickLink from "./InstagramQuickLink.jsx";
+import MultiLanguage from "./MultiLanguage.jsx";
 import "./index.css";
 import "./ux-refresh.css";
 import "./responsive-fix.css";
 import "./content-source-fix.css";
+import "./multi-language.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <InstagramQuickLink />
+    <MultiLanguage />
   </React.StrictMode>,
 );
