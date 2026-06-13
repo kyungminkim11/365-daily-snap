3716a0485a03643a9a4329dce9f2e2f49ddcb860
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-13 17:47:16 +0900

    Wire Japan-ready UX enhancements

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import InstagramQuickLink from "./InstagramQuickLink.jsx";
+import LocalePathRouter from "./LocalePathRouter.jsx";
 import MultiLanguage from "./MultiLanguage.jsx";
+import GrowthSections from "./GrowthSections.jsx";
+import PerformanceHints from "./PerformanceHints.jsx";
 import "./index.css";
 import "./ux-refresh.css";
 import "./responsive-fix.css";
 import "./content-source-fix.css";
 import "./multi-language.css";
+import "./growth-sections.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <LocalePathRouter />
     <App />
+    <GrowthSections />
     <InstagramQuickLink />
     <MultiLanguage />
+    <PerformanceHints />
   </React.StrictMode>,
 );
