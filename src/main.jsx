a753b80a92774dc0125172d1e2008e337b1225f5
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-07-11 22:57:36 +0900

    Install static-host analytics transport

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import SiteExperience, { SiteErrorBoundary } from "./SiteExperience.jsx";
+import { installAnalyticsTransport } from "./analyticsTransport.js";
 import "./index.css";
+
+installAnalyticsTransport();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
